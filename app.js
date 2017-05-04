1aafac7f684701b4b33de7fd6e1d77a258d8d33a
Ni Tongyang <ntyaiqingr@gmail.com>
2017-05-04 15:08:38 -0400

    set up the server

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello Express</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+  res.send('<h1>This is the dashboard page!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Chatapp running on the port 3000');
+});
